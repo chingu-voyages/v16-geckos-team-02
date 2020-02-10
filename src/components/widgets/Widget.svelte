8e5ab1ea890616949cfddf69b0f355aa4373b0cb
Lily <46561378+lily-law@users.noreply.github.com>
2020-02-10 16:04:20 +0000

    Create Widget.svelte

---
+++ src/components/widgets/Widget.svelte
@@ -0,0 +1,13 @@
+<script>
+    export let title = 'Not Set!';
+</script>
+
+<div>
+  <h2>{title}</h2>
+	<slot></slot> <!-- * slot -->
+</div>
+
+<style>
+</style>
+
+<!-- // * https://svelte.dev/tutorial/slots -->
