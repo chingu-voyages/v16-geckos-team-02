b8ca6b6daf606569e22a1c0cc010cb7a1f7c1983
Lily <46561378+lily-law@users.noreply.github.com>
2020-02-10 16:03:56 +0000

    Create Dash.svelte

---
+++ src/components/Dash.svelte
@@ -0,0 +1,20 @@
+<script>
+    import { getActiveDash } from '../dataStore';
+    import Sticky from './widgets/Sticky.svelte';
+    let dashData = getActiveDash();
+    let widgets = dashData ? dashData.widgets : [];
+</script>
+
+<section>
+    {#each Object.keys(widgets) as ref}
+        {#if widgets[ref].type === 'Sticky'}
+            <Sticky {ref} />
+            {:else}
+            <div>{widgets[ref].type} not yet implemented</div>
+        {/if}
+    {/each}
+</section>
+
+<style>
+
+</style>
