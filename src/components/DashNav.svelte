920014acd8b4ffde261d1151e7cc1b7405053b74
Lily <46561378+lily-law@users.noreply.github.com>
2020-02-10 22:50:21 +0000

    Update DashNav.svelte

--- src/components/DashNav.svelte
+++ src/components/DashNav.svelte
@@ -1,12 +1,11 @@
 <script>
     import { getActiveDash } from '../dataStore';
-    let title = getActiveDash().title;
+    let _title = getActiveDash()._title;
     // TODO bind title from dataStore to an input
-    console.log(title)
 </script>
 
 <nav>
-    <h1>{$title}</h1>
+    <h1>{$_title}</h1>
 </nav>
 
 <style>
