bda0d820514d5d273c11958d7541a73f400ccf09
Lily <46561378+lily-law@users.noreply.github.com>
2020-02-12 09:39:12 +0000

    Style menu

--- src/components/WidgetMenu.svelte
+++ src/components/WidgetMenu.svelte
@@ -39,5 +39,51 @@
   </nav>
   
   <style>
- 
+  nav {
+    width: 280px;
+    position: fixed;
+    bottom: 5vh;
+    right: 5vw;
+    display: inline-grid;
+    grid-template-columns: 1fr 5fr 1fr;
+    grid-template-rows: auto auto 70px;
+    grid-template-areas: 
+      ". . cancel"
+      "menu menu menu"
+      "trash title add";
+    align-items: center;
+  }
+  div {
+    grid-area: menu;
+    border: solid 1px #707070;
+  }
+  .cancel {
+    grid-area: cancel;
+    place-self: center end;
+  }
+  button {
+    width: 100%;
+    display: inline-flex;
+    flex-flow: row nowrap;
+    align-items: center;
+    background: none;
+    border: none;
+  }
+  h3 {
+    width: 100%;
+    font-size: 32px;
+    font-weight: 300;
+    margin: 0;
+  }
+  h2 {
+    grid-area: title;
+    display: inline-grid;
+    place-items: center center;
+    height: 96%;
+    border: solid 1px #707070;
+    border-width: 1px 0 1px 0;
+    font-size: 32px;
+    font-weight: 300;
+    margin: 0;
+  }
   </style>
