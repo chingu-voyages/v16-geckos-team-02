ca87ad01223da28bf8462f6916a27d37b8338b09
Lily <46561378+lily-law@users.noreply.github.com>
2020-02-12 09:37:34 +0000

    Add toggling menu functions

--- src/components/WidgetMenu.svelte
+++ src/components/WidgetMenu.svelte
@@ -1,6 +1,26 @@
 <script>
+  import { addWidget } from '../dataStore.js';
     import Add from './buttons/Add.svelte';
     import Trash from './buttons/Trash.svelte';
+  let menuOpen = false;
+  let trashActive = false;
+  const closeMenu = () => {
+    setTimeout(() => { 
+      if (menuOpen) { // timeout and latch so runs after toggle
+        menuOpen = false;
+      }
+    }, 0);
+    window.removeEventListener('click', closeMenu, {capture : true});
+  }
+  const openMenu = () => {
+    menuOpen = true;
+    window.addEventListener('click', closeMenu, {capture : true});
+  }
+  const toggleMenu = () => {menuOpen ? closeMenu() : openMenu()};
+  const add = type => {
+    addWidget(type);
+    closeMenu();
+  }
   </script>
   
   <nav>
