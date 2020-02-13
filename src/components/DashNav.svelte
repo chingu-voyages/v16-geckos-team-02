a9e4c335efab85329a323e6d60f813fd92a03ae1
Lily <46561378+lily-law@users.noreply.github.com>
2020-02-13 14:20:30 +0000

    Add editable title and buttons

--- src/components/DashNav.svelte
+++ src/components/DashNav.svelte
@@ -1,11 +1,25 @@
 <script>
     import { getActiveDash } from '../dataStore';
+    import Left from './buttons/Left.svelte';
+    import Right from './buttons/Right.svelte';
+    import Trash from './buttons/Trash.svelte';
+    import Add from './buttons/Add.svelte';
     let _title = getActiveDash()._title;
-    // TODO bind title from dataStore to an input
+    let editingTitle = false;
 </script>
 
 <nav>
-    <h1>{$_title}</h1>
+    <Left />
+    <Trash />
+    <div>
+        {#if editingTitle}
+            <input bind:value={$_title} on:blur={() => editingTitle = false} type="text" autofocus />
+        {:else}
+            <a class="active-dash-title" on:click={() => editingTitle = true}>{$_title}</a>
+        {/if}
+    </div>
+    <Add />
+    <Right />
 </nav>
 
 <style>
