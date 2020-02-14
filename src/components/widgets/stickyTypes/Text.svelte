3de25a0619b43de6dc99c3b58efc5a397b7dad78
Lily <46561378+lily-law@users.noreply.github.com>
2020-02-14 08:24:40 +0000

    Text editable

--- src/components/widgets/stickyTypes/Text.svelte
+++ src/components/widgets/stickyTypes/Text.svelte
@@ -1,10 +1,20 @@
 
 <script>
 	export let _data;
+	let editing = true;
 </script>
 
-<textarea bind:value={$_data}></textarea>
+{#if editing}
+	<textarea bind:value={$_data} on:blur={() => editing = false} autofocus />
+{:else}
+	<article on:click={() => editing = true}>{$_data}</article>
+{/if}
 
 <style>
-
+	textarea, article {
+		width: 90%;
+		background: none;
+		padding: 0;
+		margin: 0;
+	}
 </style>
