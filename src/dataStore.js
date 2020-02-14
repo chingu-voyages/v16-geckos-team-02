92c70f4d0094769139939cec36fa45281ce8a633
yanpelov <yanpelov@gmail.com>
2020-02-14 23:15:17 +0200

    Added DELETE and UPDATE functionalities for dashboards and widgets
    - Added try-catch for the CURD functions in the data store
    - Implemented delete by reference for dash and widget
    - Implemented update (for now only for a title) for dash and widget
    - Added implementation for removeSelf by calling removeWidget

--- src/dataStore.js
+++ src/dataStore.js
@@ -1,31 +1,67 @@
-import { writable } from 'svelte/store';
+import {
+    writable
+} from 'svelte/store';
 
 const dashboards = new Map();
 export const activeDashId = Symbol(); // the first dashboard key
 export const getActiveDash = () => dashboards.get(activeDashId);
 export const getWidget = ref => getActiveDash().widgets.get(ref);
-export const addDash = (title, ref = Symbol()) => {
-    const dashData = { 
-        _title: writable(title), 
-        widgets: new Map()
-    };
-    dashboards.set(ref, dashData);
-    // TODO error handling
-} 
-export const addWidget = type => {
-    const widgetData = {
-        type,
-        _title: writable(''),
-        _data: writable('')
-    }
-    getActiveDash().widgets.set(Symbol(), widgetData);
-     // TODO error handling
-} 
 
-// TODO make deleteWidget(ref) method
+export const addDash = (title, ref = Symbol()) => {
+    try {
+        const dashData = {
+            _title: writable(title),
+            widgets: new Map()
+        };
+        dashboards.set(ref, dashData);
+    } catch (e) {
+        // TODO decide how to handle the exception
+    }
+}
+export const addWidget = type => {
+    try {
+        const widgetData = {
+            type,
+            _title: writable(''),
+            _data: writable('')
+        }
+        getActiveDash().widgets.set(Symbol(), widgetData);
+    } catch (e) {
+        // TODO decide how to handle the exception
+    }
+}
+
+export const removeDash = ref => {
+    try {
+        dashboards.delete(ref);
+    } catch (e) {
+        // TODO decide how to handle the exception
+    }
+}
+
+export const removeWidget = (dashRef, widgetRef) => {
+    try {
+        dashboards.get(dashRef).widgets.delete(widgetRef);
+    } catch (e) {
+        // TODO decide how to handle the exception
+    }
+}
+
+export const updateDash = (title, ref) => {
+    try {
+        dashboards.get(ref)._title.set(title);
+    } catch (e) {
+        // TODO decide how to handle the exception
+    }
+}
+
+export const updateWidget = (title, dashRef, widgetRef) => {
+    try {
+        dashboards.get(dashRef).widgets.get(widgetRef)._title.set(title);
+    } catch (e) {
+        // TODO decide how to handle the exception
+    }
+}
 
 addDash('default dash', activeDashId);
 addWidget('Sticky');
-
-
-
