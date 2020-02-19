e5cbe65f4c24763d149829b04552c6df3597b5cd
Lily <46561378+lily-law@users.noreply.github.com>
2020-02-19 13:40:14 +0000

    setup carousel layout

--- src/components/DashNav.svelte
+++ src/components/DashNav.svelte
@@ -11,55 +11,96 @@
 <nav>
     <Left />
     <Trash />
-    <div>
-        {#if editingTitle}
-            <input bind:value={$_title} on:blur={() => editingTitle = false} type="text" autofocus />
-        {:else}
-            <a class="active-dash-title" on:click={() => editingTitle = true}>{$_title}</a>
-        {/if}
+    <div class="container">
+        <div class="carousel">
+            <button class="prev-prev-prev" on:click={() => null}>prev-prev-prev</button>
+            <button class="prev-prev" on:click={() => null}>prev-prev</button>
+            <button class="prev" on:click={() => null}>prev</button>
+            <div class="current">
+                {#if editingTitle}
+                    <input bind:value={$_title} on:blur={() => editingTitle = false} type="text" autofocus />
+                {:else}
+                    <button class="active-dash-title" on:click={() => editingTitle = true}>{$_title}</button>
+                {/if}
+            </div>
+            <button class="next" on:click={() => null}>next</button>
+            <button class="next-next" on:click={() => null}>next-next</button>
+            <button class="next-next-next" on:click={() => null}>next-next-next</button>
+        </div>
     </div>
     <Add />
     <Right />
 </nav>
 
 <style>
-nav {
+nav { 
+    width: 100%;
     display: grid;
     grid-template-columns: auto 70px auto 70px auto 70px auto 70px auto;
     grid-template-areas: ". left . trash bar add . right .";
     grid-template-rows: 70px auto;
     margin-bottom: 70px;
 }
-div {
-    overflow: hidden;
+.container {
     grid-area: bar;
+    overflow: hidden;
+    width: 100%;
+    height: 70px;
+    box-sizing: border-box;
     border: solid 1px #707070;
+}
+.carousel {
+    width: 150%;
+    height: 100%;
+    position: relative;
+    left: -25%;
     display: flex;
     flex-flow: row nowrap;
     align-items: center;
-    justify-content: center; /* will need to change when we add dashes */
+    align-content: center;
+    justify-content: space-between;
 }
-.active-dash-title, input {
-    text-decoration: none;
+button, input {
+    background: none;
+    border: none;
     color: #707070;
-    font-size: 32px;
     padding: 0;
     margin: 0;
     text-align: center;
     white-space: nowrap;
-    min-width: 120px;
     height: 50px;
 }
-@media only screen and (max-width: 640px) {
-    nav {
-    display: grid;
-    grid-template-columns: auto 70px auto 70px auto 70px auto 70px auto;
-    grid-template-areas: 
-    ". left . trash . add . right ."
-    ". bar bar bar bar bar bar bar .";
-    row-gap: 24px;
-    grid-template-rows: 70px 70px auto;
-    margin-bottom: 70px;
+.current {
+    min-width: 120px;
+    font-size: 32px;
 }
+.prev, .next {
+    font-size: 24px;
+}
+.prev-prev, .next-next {
+    font-size: 16px;
+}
+.prev-prev-prev, .next-next-next {
+    font-size: 12px;
+}
+
+@media only screen and (max-width: 768px) {
+    nav {
+        width: 110%;
+        margin-left: -5%;  
+        grid-template-areas: 
+        ". left . trash . add . right ."
+        "bar bar bar bar bar bar bar bar bar";
+        row-gap: 24px;
+        grid-template-rows: 70px 70px auto;
+        margin-bottom: 70px;
+    }
+  
+}
+@media only screen and (max-width: 420px) { 
+    .carousel {
+        width: 200%;
+        left: -50%;
+    }
 }
 </style>
